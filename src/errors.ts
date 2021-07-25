4b721ea9a6ee585ef7e0f72ffdaa233347618dc2
Benjamin Mayanja <vibenjamin6@gmail.com>
2021-07-25 17:27:44 +0300

    Add topups

---
+++ src/errors.ts
@@ -0,0 +1,11 @@
+export const handleError = (fields, err, callback) => {
+  if (err.includes('ReqValidationError')) {
+    for (let field of fields) {
+      if (err.includes(field)) {
+        return callback(`Invalid ${field}`);
+      }
+    }
+  } else {
+    callback(err);
+  }
+};
